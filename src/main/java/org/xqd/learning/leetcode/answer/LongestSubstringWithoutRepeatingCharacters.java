7992379173e78ef8536d4f359d3696bf112e6777
XuQidong <qidongxuca@gmail.com>
2021-06-07 17:50:37 +0800

    思维限制

--- src/main/java/org/xqd/learning/leetcode/answer/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/org/xqd/learning/leetcode/answer/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,7 @@
 package org.xqd.learning.leetcode.answer;
 
+import java.util.Arrays;
+
 /**
  * @author XuQidong
  * @date 2021/3/23 20:38
@@ -7,7 +9,7 @@ package org.xqd.learning.leetcode.answer;
 public class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args){
         String s = "abcbacbb";
-        int length = lengthOfLongestSubstring(s);
+        int length = lengthOfLongestSubstringN(s);
         System.out.println(length);
     }
 
@@ -15,16 +17,43 @@ public class LongestSubstringWithoutRepeatingCharacters {
         boolean[] exist = new boolean[256];
         int i = 0, maxLen = 0;
         for (int j = 0; j < s.length(); j++) {
-            System.out.println("j:" + exist[s.charAt(j)]);
+            System.out.println("j:" + j);
+            System.out.println(s.charAt(j) + ":" + exist[s.charAt(j)]);
+
             while (exist[s.charAt(j)]) {
                 exist[s.charAt(i)] = false;
-                System.out.println("i:" + exist[s.charAt(i)]);
+                System.out.println(s.charAt(i) + ":" + exist[s.charAt(i)]);
                 i++;
+                System.out.println("i:" + i);
             }
+
             exist[s.charAt(j)] = true;
+            System.out.println(s.charAt(j) + ":" + exist[s.charAt(j)]);
             maxLen = Math.max(j - i + 1, maxLen);
             System.out.println(maxLen);
             System.out.println("********");
+        }
+        return maxLen;
+    }
+
+    public static int lengthOfLongestSubstringN(String s) {
+        int[] charMap = new int[256];
+        Arrays.fill(charMap, -1);
+        int i = 0, maxLen = 0;
+        for (int j = 0; j < s.length(); j++) {
+            System.out.println("j: " + j);
+
+            // 和上一个相比，这次直接定位到重复字符的位置，而不需要考虑前面的其他字符
+            // 因为前面的字符也没有用了
+            // 这个有点难啊
+            // 这里需要处理字符串的各种类型
+            if (charMap[s.charAt(j)] >= i) {
+                i = charMap[s.charAt(j)] + 1;
+                System.out.println("i: " + i);
+            }
+            charMap[s.charAt(j)] = j;
+            maxLen = Math.max(j - i + 1, maxLen);
+            System.out.println("*********");
         }
         return maxLen;
     }
--- src/main/java/org/xqd/learning/leetcode/answer/SwappingNodesInLinkedList.java
+++ src/main/java/org/xqd/learning/leetcode/answer/SwappingNodesInLinkedList.java
@@ -9,26 +9,66 @@ import org.xqd.learning.leetcode.pojo.ListNode;
 public class SwappingNodesInLinkedList {
 
     public static void main(String[] args) throws InterruptedException {
-//        ListNode node9 = new ListNode(9, null);
-//        ListNode node8 = new ListNode(9, node9);
-        ListNode node7 = new ListNode(0, null);
-        ListNode node6 = new ListNode(3, node7);
-        ListNode node5 = new ListNode(8, node6);
-        ListNode node4 = new ListNode(1, node5);
-        ListNode node3 = new ListNode(6, node4);
-        ListNode node2 = new ListNode(8, node3);
-        ListNode node1 = new ListNode(9, node2);
-        ListNode head = new ListNode(7, node1);
+        ListNode node6 = new ListNode(7, null);
+        ListNode node5 = new ListNode(6, node6);
+        ListNode node4 = new ListNode(5, node5);
+        ListNode node3 = new ListNode(4, node4);
+        ListNode node2 = new ListNode(3, node3);
+        ListNode node1 = new ListNode(2, node2);
+        ListNode head = new ListNode(1, node1);
 
-        ListNode node = swapNodes(head, 8);
+        //index = 1,2,3,4
+        ListNode node = swapNodes(head, 2);
         while (node != null) {
             System.out.println(node.val);
             node = node.next;
-            Thread.sleep(20);
+            Thread.sleep(1000);
         }
     }
 
     public static ListNode swapNodes(ListNode head, int k) {
+
+        if(head.next == null && k == 1)
+        {
+            return head;
+        }
+
+        int length = 0;
+        ListNode headL = head;
+        //figure out the number of linked list
+        while(headL != null)
+        {
+            length++;
+            headL = headL.next;
+        }
+        System.out.println("当前链表长度为: " + length);
+
+        //figure out two node position
+        int p = length - k + 1;
+        int cp = 1;
+        ListNode firstNode = null;
+        ListNode secondeNode = null;
+
+        ListNode tempHead = head;
+        while(tempHead != null)
+        {
+            if(cp == k)
+            {
+                firstNode = tempHead;
+            }
+
+            if(cp == p)
+            {
+                secondeNode = tempHead;
+            }
+
+            cp++;
+            tempHead = tempHead.next;
+        }
+
+        int value = secondeNode.val;
+        secondeNode.val = firstNode.val;
+        firstNode.val = value;
 
         return head;
     }
