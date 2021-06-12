93bba4125f502bfeff36da876db8268426703236
XuQidong <qidongxuca@gmail.com>
2021-06-13 07:12:42 +0800

    what is the problem?

--- src/main/java/org/xqd/learning/leetcode/answer/RemoveLinkedListElements.java
+++ src/main/java/org/xqd/learning/leetcode/answer/RemoveLinkedListElements.java
@@ -1,5 +1,45 @@
-package org.xqd.learning.leetcode.answer;/**
+package org.xqd.learning.leetcode.answer;
+
+import org.xqd.learning.leetcode.pojo.ListNode;
+
+/**
  * @author XuQidong
  * @date 2021/6/12 21:47
- */public class RemoveLinkedListElements {
+ */
+public class RemoveLinkedListElements {
+    public static void main(String[] args) throws InterruptedException {
+        ListNode node6 = new ListNode(2, null);
+        ListNode node5 = new ListNode(2, node6);
+        ListNode node4 = new ListNode(2, node5);
+        ListNode node3 = new ListNode(2, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(2, node2);
+        ListNode head = new ListNode(2, node1);
+
+        ListNode listNode = removeElements(head, 2);
+        while (listNode != null) {
+            System.out.println(listNode.val);
+            listNode = listNode.next;
+            Thread.sleep(1000);
+        }
+
+    }
+
+    public static ListNode removeElements(ListNode head, int val) {
+        ListNode dummyNode = new ListNode(0);
+        ListNode pre = dummyNode;
+        pre.next = head;
+
+        while (head != null) {
+            if (head.val == val) {
+                pre.next = head.next;
+                head = head.next;
+            } else {
+                pre = head;
+                head = head.next;
+            }
+        }
+
+        return dummyNode.next;
+    }
 }
