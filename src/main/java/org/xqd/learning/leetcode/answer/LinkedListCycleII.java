8dea317f917bd5ee3523908059d651628cf0d077
XuQidong <qidongxuca@gmail.com>
2021-06-12 21:43:27 +0800

    20210612

---
+++ src/main/java/org/xqd/learning/leetcode/answer/LinkedListCycleII.java
@@ -0,0 +1,23 @@
+package org.xqd.learning.leetcode.answer;
+
+import org.xqd.learning.leetcode.pojo.ListNode;
+
+/**
+ * @author XuQidong
+ * @date 2021/6/12 20:56
+ */
+public class LinkedListCycleII {
+    public static void main(String[] args){
+
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode dummyNode = new ListNode(0);
+        ListNode pre = dummyNode;
+        pre.next = head;
+
+
+
+        return dummyNode.next;
+    }
+}
