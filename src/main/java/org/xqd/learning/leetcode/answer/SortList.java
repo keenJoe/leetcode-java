21c29cdc8c58c1218c7d682156bbd3fc96eae086
XuQidong <qidongxuca@gmail.com>
2021-06-18 16:20:16 +0800

    update remove linked list elements

--- src/main/java/org/xqd/learning/leetcode/answer/SortList.java
+++ src/main/java/org/xqd/learning/leetcode/answer/SortList.java
@@ -7,8 +7,24 @@ import org.xqd.learning.leetcode.pojo.ListNode;
  * @date 2021/6/13 20:54
  */
 public class SortList {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
+        ListNode node7 = new ListNode(8, null);
+        ListNode node6 = new ListNode(9, node7);
+        ListNode node5 = new ListNode(4, node6);
+        ListNode node4 = new ListNode(6, node5);
+        ListNode node3 = new ListNode(2, node4);
+        ListNode node2 = new ListNode(4, node3);
+        ListNode node1 = new ListNode(3, node2);
+        ListNode head = new ListNode(1, node1);
 
+        SortList sortList = new SortList();
+        ListNode listNode = sortList.sortList(head);
+
+        while(listNode != null){
+            System.out.println(listNode.val);
+            listNode = listNode.next;
+            Thread.sleep(1000);
+        }
     }
 
     public ListNode sortList(ListNode head) {
