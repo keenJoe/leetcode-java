46aa76fd9f2e14efee39e04b818d69a920e7fed9
XuQidong <qidongxuca@gmail.com>
2021-07-09 17:13:35 +0800

    two sum

--- src/main/java/org/xqd/learning/leetcode/answer/TwoSum.java
+++ src/main/java/org/xqd/learning/leetcode/answer/TwoSum.java
@@ -1,11 +1,30 @@
 package org.xqd.learning.leetcode.answer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author XuQidong
  * @date 2021/1/26 10:20
  */
 public class TwoSum {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        int[] array = new int[]{2, 4, 5};
+        int[] twoSum = twoSum(array, 7);
+        for (int i : twoSum) {
+            System.out.println(i);
+        }
+    }
 
+    public static int[] twoSum(int[] numbers, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            int x = numbers[i];
+            if (map.containsKey(target - x)) {
+                return new int[]{map.get(target - x), i};
+            }
+            map.put(x, i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
