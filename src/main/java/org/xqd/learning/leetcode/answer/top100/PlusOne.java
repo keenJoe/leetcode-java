62b7c3fc54fa9d7c00b2be2d9e36062dd1f2333e
XuQidong <qidongxuca@gmail.com>
2022-06-21 07:03:25 +0800

    finish isBalanced

---
+++ src/main/java/org/xqd/learning/leetcode/answer/top100/PlusOne.java
@@ -0,0 +1,15 @@
+package org.xqd.learning.leetcode.answer.top100;
+
+/**
+ * LeetCode 66. Plus One
+ */
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
