3cd80ec793826db56810fcd13ed300499706ef4a
XuQidong <qidongxuca@gmail.com>
2023-01-10 22:45:57 +0800

    add new code

---
+++ src/main/java/org/xqd/learning/leetcode/answer/linkedlist/MergeTwoSortedLists.java
@@ -0,0 +1,13 @@
+package org.xqd.learning.leetcode.answer.linkedlist;
+
+
+import org.xqd.learning.leetcode.pojo.ListNode;
+
+/**
+ * 21. Merge Two Sorted Lists
+ */
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        return null;
+    }
+}
