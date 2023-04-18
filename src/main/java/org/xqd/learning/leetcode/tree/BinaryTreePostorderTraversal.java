74a8379c4422bbd1ed1a5f6813c25f9a9c7fe877
XuQidong <qidongxuca@gmail.com>
2023-04-19 07:33:17 +0800

    144. Binary Tree Preorder Traversal, using statck to imitate recursion

---
+++ src/main/java/org/xqd/learning/leetcode/tree/BinaryTreePostorderTraversal.java
@@ -0,0 +1,14 @@
+package org.xqd.learning.leetcode.tree;
+
+import org.xqd.learning.leetcode.pojo.TreeNode;
+
+import java.util.List;
+
+/**
+ * 145. Binary Tree Postorder Traversal
+ */
+public class BinaryTreePostorderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        return null;
+    }
+}
---
+++ src/main/java/org/xqd/learning/leetcode/tree/BinaryTreePreorderTraversal.java
@@ -0,0 +1,30 @@
+package org.xqd.learning.leetcode.tree;
+
+import org.xqd.learning.leetcode.pojo.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 144. Binary Tree Preorder Traversal
+ */
+public class BinaryTreePreorderTraversal {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                list.add(root.val);
+                stack.add(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+            root = root.right;
+        }
+
+        return list;
+    }
+}
