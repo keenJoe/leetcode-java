ba7cdf92c56c07b6b236d3ea184ac71bec1ca6b8
XuQidong <qidongxuca@gmail.com>
2023-04-21 13:25:46 +0800

    add MaximumDepthOfBinaryTree

---
+++ src/main/java/org/xqd/learning/leetcode/tree/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,14 @@
+package org.xqd.learning.leetcode.tree;
+
+import org.xqd.learning.leetcode.pojo.TreeNode;
+
+/**
+ * 104. Maximum Depth of Binary Tree
+ */
+public class MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
