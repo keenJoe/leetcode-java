458534b9eeff4a86576f1fc7bc7abffa2e5a08a7
XuQidong <qidongxuca@gmail.com>
2023-05-05 14:14:18 +0800

    finish 160

--- src/main/java/org/xqd/learning/leetcode/linkedlist/IntersectionOfTwoLinkedLists.java
+++ src/main/java/org/xqd/learning/leetcode/linkedlist/IntersectionOfTwoLinkedLists.java
@@ -13,6 +13,46 @@ public class IntersectionOfTwoLinkedLists {
      * 3 ToDo
      */
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+
+        int aLength = linkedListLenth(headA);
+        int bLength = linkedListLenth(headB);
+
+        //headA 的长度大于 headB 的长度
+        if (aLength - bLength > 0) {
+            while (aLength - bLength > 0) {
+                headA = headA.next;
+                aLength--;
+            }
+        } else {
+            while (bLength - aLength > 0) {
+                headB = headB.next;
+                bLength--;
+            }
+        }
+
+        //到这里headA和headB的长度已经拉齐
+
+        while (headA != null && headB != null) {
+            if (headA == headB) {
+                return headA;
+            }
+
+            headA = headA.next;
+            headB = headB.next;
+        }
+
         return null;
+    }
+
+    public int linkedListLenth(ListNode head){
+        int length = 0;
+
+        while (head != null) {
+            length += 1;
+            head = head.next;
+        }
+
+        return length;
     }
 }
