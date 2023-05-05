6deebc0d2dab1c9da97cdddb7e9b7aa0c9d41c31
XuQidong <qidongxuca@gmail.com>
2023-05-05 08:48:22 +0800

    add two new questions

---
+++ src/main/java/org/xqd/learning/leetcode/linkedlist/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,17 @@
+package org.xqd.learning.leetcode.linkedlist;
+
+import org.xqd.learning.leetcode.pojo.ListNode;
+
+/**
+ * 160. Intersection of Two Linked Lists
+ */
+public class IntersectionOfTwoLinkedLists {
+    /**
+     * 1 想到的第一种方案是从后向前找intersection
+     * 2 分别统计出两个链表的长度，然后再找两个链表的intersection
+     * 3 ToDo
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        return null;
+    }
+}
---
+++ src/main/java/org/xqd/learning/leetcode/linkedlist/RemoveLinkedListElements.java
@@ -0,0 +1,12 @@
+package org.xqd.learning.leetcode.linkedlist;
+
+import org.xqd.learning.leetcode.pojo.ListNode;
+
+/**
+ * 203. Remove Linked List Elements
+ */
+public class RemoveLinkedListElements {
+    public ListNode removeElements(ListNode head, int val) {
+        return null;
+    }
+}
