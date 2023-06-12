b97b09e3ece34f71fd33a303024f919e8df41ee6
XuQidong <qidongxuca@gmail.com>
2023-06-12 21:40:44 +0800

    two sum after soting

--- src/main/java/org/xqd/learning/leetcode/array/TwoSum.java
+++ src/main/java/org/xqd/learning/leetcode/array/TwoSum.java
@@ -1,5 +1,6 @@
 package org.xqd.learning.leetcode.array;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,11 +9,55 @@ import java.util.Map;
  */
 public class TwoSum {
     /**
+     * @param nums
+     * @param target
+     * @return
+     *
+     * 但是这个不能通过排序啊😭
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            int j = bsearch(nums, target - nums[i], i + 1);
+            if (j != -1) {
+                return new int[]{i, j};
+            }
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /**
+     * 二分法查找
+     *
+     * @param nums
+     * @param i
+     * @param i1
+     * @return
+     */
+    private int bsearch(int[] nums, int key, int start) {
+        int L = start;
+        int R = nums.length;
+        while (L < R) {
+            int M = L + (R - L) / 2;
+            //key is on the left of M
+            if (nums[M] > key) {
+                R = M;
+            } else {
+                L = M;
+            }
+        }
+
+        return (L == R && nums[L] == key) ? L : -1;
+    }
+
+
+    /**
      * [3,2,3]
      * 6
      * 没有考虑到这种情况
      */
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum2(int[] nums, int target) {
         Map<Integer, Integer> value = new HashMap<>();
         int[] indexs = new int[2];
 
