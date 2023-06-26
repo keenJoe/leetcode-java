ca31b311301ef827ecddeff0be18497b72864ef9
XuQidong <qidongxuca@gmail.com>
2023-06-26 15:35:31 +0800

    add new question

---
+++ src/main/java/org/xqd/learning/leetcode/linkedlist/MiddleOfTheLinkedList.java
@@ -0,0 +1,14 @@
+package org.xqd.learning.leetcode.linkedlist;
+
+import org.xqd.learning.leetcode.pojo.ListNode;
+
+/**
+ * @description: 876. Middle of the Linked List
+ * @author: Joe
+ * @create: 2023-06-26 15:30
+ **/
+public class MiddleOfTheLinkedList {
+    public ListNode middleNode(ListNode head) {
+        return null;
+    }
+}
