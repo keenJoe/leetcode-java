8a8f136cf0f05ac4f6a10fbaffad7b2d033a9a03
XuQidong <qidongxuca@gmail.com>
2023-07-01 22:05:58 +0800

    finish 24

--- src/main/java/org/xqd/learning/leetcode/linkedlist/SwapNodesInPairs.java
+++ src/main/java/org/xqd/learning/leetcode/linkedlist/SwapNodesInPairs.java
@@ -7,6 +7,43 @@ import org.xqd.learning.leetcode.pojo.ListNode;
  */
 public class SwapNodesInPairs {
     public ListNode swapPairs(ListNode head) {
-        return null;
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode dummyNode = new ListNode();
+        ListNode pre = null;
+
+        while (head != null) {
+            ListNode temp = head;
+            ListNode headNext = temp.next;
+            head.next = null;
+
+            ListNode nextHead = null;
+            if (headNext != null) {
+                nextHead = headNext.next;
+            }
+
+            if (dummyNode.next == null) {
+                dummyNode.next = headNext;
+            }
+
+            if (pre != null && headNext != null) {
+                pre.next = headNext;
+            } else if (pre != null) {
+                pre.next = temp;
+            }
+
+            pre = temp;
+
+            if (headNext != null) {
+                headNext.next = temp;
+            }
+
+            temp.next = nextHead;
+            head = nextHead;
+        }
+
+        return dummyNode.next;
     }
 }
