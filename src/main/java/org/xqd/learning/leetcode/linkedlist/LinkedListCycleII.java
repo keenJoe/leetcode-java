55fcd0a4391f449c12072a232e1478c360146776
XuQidong <qidongxuca@gmail.com>
2023-07-01 23:14:07 +0800

    finish 142 using fast and slow pointer

---
+++ src/main/java/org/xqd/learning/leetcode/linkedlist/LinkedListCycleII.java
@@ -0,0 +1,37 @@
+package org.xqd.learning.leetcode.linkedlist;
+
+import org.xqd.learning.leetcode.pojo.ListNode;
+
+/**
+ * 142. Linked List Cycle II
+ */
+public class LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        /**
+         * slow 和 fast 一定要从同一个node开始走
+         */
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                slow = head;
+
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+
+                return slow;
+        }
+
+        return null;
+    }
+}
