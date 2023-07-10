f94d9e12c5280fd3456f98b7fc6033e5c70c9629
XuQidong <qidongxuca@gmail.com>
2023-07-11 07:46:35 +0800

    finish 147

--- src/main/java/org/xqd/learning/leetcode/linkedlist/InsertionSortList.java
+++ src/main/java/org/xqd/learning/leetcode/linkedlist/InsertionSortList.java
@@ -7,25 +7,37 @@ import org.xqd.learning.leetcode.pojo.ListNode;
  */
 public class InsertionSortList {
     public ListNode insertionSortList(ListNode head) {
-        if (head == null || head.next == null) return head;
+        if (head == null || head.next == null) {
+            return head;
+        }
 
-        ListNode dummyNode= new ListNode();
-        ListNode pre = head;
-        dummyNode.next = pre;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = head;
+        ListNode curr = head.next;
 
-        pre.next = null;
+        while (curr != null) {
+            if (curr.val < prev.val) {
+                ListNode temp = dummy;
+                while (temp.next.val < curr.val) {
+                    temp = temp.next;
+                }
 
-        ListNode cur = head.next;
+                //跳过cur节点，和后面的节点连接
+                prev.next = curr.next;
 
-        while (cur != null) {
-            while (pre.val > cur.val) {
-                ListNode temp = pre;
-                pre = cur;
-                pre.next = temp;
-                cur = temp;
+                //temp比cur小，但是temp.next比cur大
+                curr.next = temp.next;
+                temp.next = curr;
+
+                //回到未排序的链表的第一个节点
+                curr = prev.next;
+            } else {
+                prev = curr;
+                curr = curr.next;
             }
         }
 
-        return dummyNode.next;
+        return dummy.next;
     }
 }
