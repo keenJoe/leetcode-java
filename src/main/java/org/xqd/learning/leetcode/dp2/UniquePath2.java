d8a85ee26161e6a2ba4ea96d4d2a3a36723cf89f
XuQidong <qidongxuca@gmail.com>
2023-08-11 21:57:38 +0800

    add unique path2 using dp2

--- src/main/java/org/xqd/learning/leetcode/dp2/UniquePath2.java
+++ src/main/java/org/xqd/learning/leetcode/dp2/UniquePath2.java
@@ -3,11 +3,36 @@ package org.xqd.learning.leetcode.dp2;
 public class UniquePath2 {
 
     /**
-     * 基于递归进行改造
+     * 基于上一个dp进行简化
      * @param obstacleGrid
      * @return
      */
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        if (obstacleGrid[m - 1][n - 1] == 1) {
+            return 0;
+        }
+
+        int[][] arr = new int[m + 1][n + 1];
+        arr[m - 1][n] = 1;
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                arr[i][j] = (obstacleGrid[i][j] == 1) ? 0 : arr[i][j + 1] + arr[i + 1][j];
+            }
+        }
+
+        return arr[0][0];
+    }
+
+    /**
+     * 基于递归进行改造
+     * @param obstacleGrid
+     * @return
+     */
+    public int uniquePathsWithObstacles2(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
 
