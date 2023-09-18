4eee59406c7b1e78453b5f241610dba23bdd4670
XuQidong <qidongxuca@gmail.com>
2023-09-18 20:58:35 +0800

    09xx

---
+++ src/main/java/org/xqd/learning/leetcode/array/QueensThatCanAttackTheKing.java
@@ -0,0 +1,12 @@
+package org.xqd.learning.leetcode.array;
+
+import java.util.List;
+
+/**
+ * 1222. Queens That Can Attack the King
+ */
+public class QueensThatCanAttackTheKing {
+    public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
+        return null;
+    }
+}
