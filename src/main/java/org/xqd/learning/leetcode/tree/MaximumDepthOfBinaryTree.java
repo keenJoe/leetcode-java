7d03384ccdc8e828c76b59534a2f658ffc145a49
XuQidong <qidongxuca@gmail.com>
2023-11-04 09:43:58 +0800

    104

--- src/main/java/org/xqd/learning/leetcode/tree/MaximumDepthOfBinaryTree.java
+++ src/main/java/org/xqd/learning/leetcode/tree/MaximumDepthOfBinaryTree.java
@@ -2,8 +2,12 @@ package org.xqd.learning.leetcode.tree;
 
 import org.xqd.learning.leetcode.pojo.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 104. Maximum Depth of Binary Tree
+ *
  * @author qidongxu
  */
 public class MaximumDepthOfBinaryTree {
@@ -13,5 +17,31 @@ public class MaximumDepthOfBinaryTree {
         }
 
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    public int maxDepthWithBreadth(TreeNode root) {
+        if (root == null) return 0;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        int depth = 0;
+        while (queue.size() > 0) {
+            int size = queue.size();
+            while (size > 0) {
+                TreeNode cur = queue.poll();
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+
+                size--;
+            }
+            depth++;
+        }
+        return depth;
     }
 }
