72a650851519397423abdc0daef9dbffe4a2bef1
XuQidong <qidongxuca@gmail.com>
2023-11-19 13:28:02 +0800

    1119

--- src/main/java/org/xqd/learning/leetcode/tree/BinaryTreeLevelOrderTraversalII.java
+++ src/main/java/org/xqd/learning/leetcode/tree/BinaryTreeLevelOrderTraversalII.java
@@ -7,21 +7,22 @@ import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
 
-/**
- * 102. Binary Tree Level Order Traversal
- */
-public class BinaryTreeLevelOrderTraversal {
-    public List<List<Integer>> levelOrder(TreeNode root) {
+public class BinaryTreeLevelOrderTraversalII {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
         List<List<Integer>> all = new ArrayList<>();
-
         if (root == null) return all;
+
+        Deque<List<Integer>> allDeque = new ArrayDeque<>();
 
         Deque<TreeNode> deque = new ArrayDeque<>();
         deque.add(root);
+        List<Integer> list = new ArrayList<>();
+        list.add(root.val);
+        allDeque.addFirst(list);
 
         while (!deque.isEmpty()) {
             int size = deque.size();
-            List<Integer> list = new ArrayList<>();
+            list = new ArrayList<>();
             for (int i = 0; i < size; i++) {
                 TreeNode node = deque.pollFirst();
                 list.add(node.val);
@@ -34,7 +35,11 @@ public class BinaryTreeLevelOrderTraversal {
                 }
             }
 
-            all.add(list);
+            allDeque.addFirst(list);
+        }
+
+        for (int i = 0; i < allDeque.size(); i++) {
+            all.add(allDeque.pollFirst());
         }
 
         return all;
