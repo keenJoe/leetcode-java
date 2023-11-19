9f6df397afdca9cfab0a2ea66a5cc363a5571626
XuQidong <qidongxuca@gmail.com>
2023-11-19 10:00:56 +0800

    using morris to resolve min depnth

--- src/main/java/org/xqd/learning/leetcode/tree/MinimumDepthOfBinaryTree.java
+++ src/main/java/org/xqd/learning/leetcode/tree/MinimumDepthOfBinaryTree.java
@@ -23,6 +23,53 @@ public class MinimumDepthOfBinaryTree {
     }
 
     public int minDepth(TreeNode root) {
-        return 0;
+        if (root == null) {
+            return 0;
+        }
+        TreeNode cur = root;
+        TreeNode mostRight = null;
+        //当前的层级
+        int curLevel = 0;
+        //最小深度
+        int minHeight = Integer.MAX_VALUE;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null) {
+                int rightBoardSize = 1;
+                while (mostRight.right != null && mostRight.right != cur) {
+                    rightBoardSize++;
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right == null) { // 第一次到达
+                    curLevel++;
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else { // 第二次到达
+                    //右指针不为空，但是左指针为空，而且还能到达这里，说明这个右指针是人为修改的，后增加的右指针
+                    //判断是否是叶节点
+                    if (mostRight.left == null) {
+                        minHeight = Math.min(minHeight, curLevel);
+                    }
+                    curLevel -= rightBoardSize;
+                    mostRight.right = null;
+                }
+            } else { // 只有一次到达
+                curLevel++;
+            }
+            cur = cur.right;
+        }
+
+        //单独检查最后一个右节点是不是叶节点
+        int finalRight = 1;
+        cur = root;
+        while (cur.right != null) {
+            finalRight++;
+            cur = cur.right;
+        }
+        if (cur.left == null && cur.right == null) {
+            minHeight = Math.min(minHeight, finalRight);
+        }
+        return minHeight;
     }
 }
