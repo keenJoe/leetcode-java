651c3516ebe2c34e66c855683e256c13f3ae92fa
XuQidong <qidongxuca@gmail.com>
2023-12-10 11:55:46 +0800

    108

--- src/main/java/org/xqd/learning/leetcode/tree/ConvertSortedArray2BinarySearchTree.java
+++ src/main/java/org/xqd/learning/leetcode/tree/ConvertSortedArray2BinarySearchTree.java
@@ -13,21 +13,36 @@ public class ConvertSortedArray2BinarySearchTree {
 
     /**
      * 能用递归解决的问题也一定能用迭代解决
+     *
      * @param nums
      * @return
      */
 
+//    public TreeNode sortedArrayToBST(int[] nums) {
+//        if (nums == null || nums.length == 0) return null;
+//
+//        int mid = nums.length / 2;
+//        TreeNode root = new TreeNode(nums[mid]);
+//        int[] a = Arrays.copyOfRange(nums, 0, mid);
+//        int[] b = Arrays.copyOfRange(nums, mid + 1, nums.length);
+//
+//        root.left = sortedArrayToBST(a);
+//        root.right = sortedArrayToBST(b);
+//
+//        return root;
+//    }
     public TreeNode sortedArrayToBST(int[] nums) {
-        if (nums == null || nums.length == 0) return null;
+        return helper(nums, 0, nums.length);
+    }
 
-        int mid = nums.length / 2;
+    private TreeNode helper(int[] nums, int start, int end) {
+        if (start > end) return null;
+
+        int mid = (start + end) / 2;
+
         TreeNode root = new TreeNode(nums[mid]);
-        int[] a = Arrays.copyOfRange(nums, 0, mid);
-        int[] b = Arrays.copyOfRange(nums, mid + 1, nums.length);
-
-        root.left = sortedArrayToBST(a);
-        root.right = sortedArrayToBST(b);
-
+        root.left = helper(nums, start, mid - 1);
+        root.right = helper(nums, mid + 1, end);
         return root;
     }
 }
