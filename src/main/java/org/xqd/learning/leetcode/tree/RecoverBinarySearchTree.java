81e25433dd26922fd78a28e74fdf7a8fb130b18a
XuQidong <qidongxuca@gmail.com>
2024-01-20 10:13:43 +0800

    reverse binary search tree

--- src/main/java/org/xqd/learning/leetcode/tree/RecoverBinarySearchTree.java
+++ src/main/java/org/xqd/learning/leetcode/tree/RecoverBinarySearchTree.java
@@ -3,7 +3,31 @@ package org.xqd.learning.leetcode.tree;
 import org.xqd.learning.leetcode.pojo.TreeNode;
 
 public class RecoverBinarySearchTree {
-    public void recoverTree(TreeNode root) {
 
+    TreeNode first = null;
+    TreeNode second = null;
+    TreeNode last = null;
+
+    public void recoverTree(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        dfs(root);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    public void dfs(TreeNode root) {
+        if (root == null) return;
+        dfs(root.left);
+        if (last != null && last.val > root.val) {
+            if (first == null)
+                first = last;
+            second = root;
+        }
+        last = root;
+        dfs(root.right);
     }
 }
