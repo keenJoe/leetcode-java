668c23bd645f9df968b51d778e946b9f08595991
XuQidong <qidongxuca@gmail.com>
2024-03-13 06:58:04 +0800

    0307

--- src/main/java/org/xqd/learning/leetcode/math/FindTheDivisibilityArrayString.java
+++ src/main/java/org/xqd/learning/leetcode/math/FindTheDivisibilityArrayString.java
@@ -14,12 +14,12 @@ public class FindTheDivisibilityArrayString {
     }
 
     public static int[] divisibilityArray(String word, int m) {
-        int len = word.length();
-        int[] div = new int[len];
-
-        Map<Integer, Long> map = new HashMap<>();
-        for (int i = 0; i < len; i++) {
-            long num = 0;
+//        int len = word.length();
+//        int[] div = new int[len];
+//
+//        Map<Integer, Long> map = new HashMap<>();
+//        for (int i = 0; i < len; i++) {
+//            long num = 0;
 //            for (int j = i; j >=0; j--) {
 //                num = num * 10 + word.charAt(j) - '0';
 //                System.out.println(num);
@@ -33,16 +33,33 @@ public class FindTheDivisibilityArrayString {
 //                    div[j] = 1;
 //                }
 //            }
+//            int j = word.charAt(i) - '0';
+//            if (i == 0) {
+//                num = j;
+//            } else {
+//                num = map.get(i - 1) * 10 + j;
+//            }
+//
+//            map.put(i, num);
+//
+//            if (num % m == 0) {
+//                div[i] = 1;
+//            }
+//        }
+//
+//        return div;
+
+        int len = word.length();
+        int[] div = new int[len];
+
+        Map<Integer, Long> map = new HashMap<>();
+        long num = 0;
+        for (int i = 0; i < len; i++) {
+
             int j = word.charAt(i) - '0';
-            if (i == 0) {
-                num = j;
-            } else {
-                num = map.get(i - 1) * 10 + j;
-            }
+            num = (num * 10 + j) % m;
 
-            map.put(i, num);
-
-            if (num % m == 0) {
+            if (num == 0) {
                 div[i] = 1;
             }
         }
