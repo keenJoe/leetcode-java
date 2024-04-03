d712dd443c8adea8105af82069db515bf9680b90
XuQidong <qidongxuca@gmail.com>
2024-04-03 17:16:04 +0800

    105

--- src/main/java/org/xqd/learning/leetcode/tree/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ src/main/java/org/xqd/learning/leetcode/tree/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -5,6 +5,7 @@ import org.xqd.learning.leetcode.pojo.TreeNode;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 /**
  * 105. Construct Binary Tree from Preorder and Inorder Traversal
@@ -41,6 +42,35 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
         root.left = build(preorder, inorder, rootIndex + 1, rootIndex + step, inLeft, inorderRootIndex - 1);
         //构建右子树
         root.right = build(preorder, inorder, rootIndex + step + 1, preRight, inorderRootIndex + 1, inRight);
+
+        return root;
+    }
+
+    public TreeNode buildTree1(int[] preorder, int[] inorder) {
+        if (preorder.length == 0) return null;
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode root = new TreeNode(preorder[0]);
+        stack.add(root);
+        int indexInorder = 0;
+        for (int i = 1; i < preorder.length; i++) {
+            int preValue = preorder[i];
+            TreeNode node = stack.peek();
+            if (node.val != inorder[indexInorder]) {
+                node.left = new TreeNode(preValue);
+                stack.add(node.left);
+            } else {
+                //while循环中不能用node，因为node会变化。需要每次拿栈顶的node
+                //要用stack.peek().val
+                while (!stack.isEmpty() && node.val == inorder[indexInorder]) {
+                    node = stack.pop();
+                    indexInorder++;
+                }
+
+                node.right = new TreeNode(preValue);
+                stack.add(node.right);
+            }
+        }
 
         return root;
     }
