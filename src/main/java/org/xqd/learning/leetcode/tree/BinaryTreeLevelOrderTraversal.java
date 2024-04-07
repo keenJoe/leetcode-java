2ad20b4b24a27f911a6354a00430b59242aaa870
XuQidong <qidongxuca@gmail.com>
2024-04-07 21:09:48 +0800

    102

--- src/main/java/org/xqd/learning/leetcode/tree/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/org/xqd/learning/leetcode/tree/BinaryTreeLevelOrderTraversal.java
@@ -2,16 +2,41 @@ package org.xqd.learning.leetcode.tree;
 
 import org.xqd.learning.leetcode.pojo.TreeNode;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
+import java.util.*;
 
 /**
  * 102. Binary Tree Level Order Traversal
  */
 public class BinaryTreeLevelOrderTraversal {
     public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (root == null) return list;
+
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        deque.add(root);
+
+        while (!deque.isEmpty()) {
+            int size = deque.size();
+            List<Integer> childList = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = deque.pollFirst();
+                childList.add(node.val);
+                if (node.left != null) {
+                    deque.addLast(node.left);
+                }
+
+                if (node.right != null) {
+                    deque.addLast(node.right);
+                }
+            }
+            list.add(childList);
+        }
+
+        return list;
+    }
+
+
+    public List<List<Integer>> levelOrder1(TreeNode root) {
         List<List<Integer>> all = new ArrayList<>();
 
         if (root == null) return all;
---
+++ src/main/java/org/xqd/learning/leetcode/tree/ConstructBinaryTreeFromPreorderAndPostorderTraversal.java
@@ -0,0 +1,12 @@
+package org.xqd.learning.leetcode.tree;
+
+import org.xqd.learning.leetcode.pojo.TreeNode;
+
+/**
+ * 889. Construct Binary Tree from Preorder and Postorder Traversal
+ */
+public class ConstructBinaryTreeFromPreorderAndPostorderTraversal {
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        return null;
+    }
+}
