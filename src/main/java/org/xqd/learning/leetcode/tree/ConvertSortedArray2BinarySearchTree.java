16cb15cdb35a20fd5d62d828999804ff8696a9dc
XuQidong <qidongxuca@gmail.com>
2024-04-12 02:10:15 +0800

    108

--- src/main/java/org/xqd/learning/leetcode/tree/ConvertSortedArray2BinarySearchTree.java
+++ src/main/java/org/xqd/learning/leetcode/tree/ConvertSortedArray2BinarySearchTree.java
@@ -17,29 +17,13 @@ public class ConvertSortedArray2BinarySearchTree {
      * @param nums
      * @return
      */
-
-//    public TreeNode sortedArrayToBST(int[] nums) {
-//        if (nums == null || nums.length == 0) return null;
-//
-//        int mid = nums.length / 2;
-//        TreeNode root = new TreeNode(nums[mid]);
-//        int[] a = Arrays.copyOfRange(nums, 0, mid);
-//        int[] b = Arrays.copyOfRange(nums, mid + 1, nums.length);
-//
-//        root.left = sortedArrayToBST(a);
-//        root.right = sortedArrayToBST(b);
-//
-//        return root;
-//    }
     public TreeNode sortedArrayToBST(int[] nums) {
         return helper(nums, 0, nums.length);
     }
 
     private TreeNode helper(int[] nums, int start, int end) {
         if (start > end) return null;
-
         int mid = (start + end) / 2;
-
         TreeNode root = new TreeNode(nums[mid]);
         root.left = helper(nums, start, mid - 1);
         root.right = helper(nums, mid + 1, end);
