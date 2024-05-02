d4e27c43aa02cb50bac1bd06be5b99d33114d029
XuQidong <qidongxuca@gmail.com>
2024-05-02 19:44:35 +0800

    99

--- src/main/java/org/xqd/learning/leetcode/tree/PathSum.java
+++ src/main/java/org/xqd/learning/leetcode/tree/PathSum.java
@@ -1,9 +1,6 @@
 package org.xqd.learning.leetcode.tree;
 
-import org.xqd.learning.leetcode.pojo.ListNode;
 import org.xqd.learning.leetcode.pojo.TreeNode;
-
-import java.util.*;
 
 /**
  * 112. Path Sum
@@ -11,97 +8,57 @@ import java.util.*;
  * @author qidongxu
  */
 public class PathSum {
-    public static void main(String[] args) {
-//        TreeNode head = new TreeNode(5);
-//        head.left = new TreeNode(4);
-//        head.right = new TreeNode(8);
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+//        if (root != null && root.val == targetSum && (root.left != null || root.right != null)) return false;
 //
-//        head.left.left = new TreeNode(11);
-//        head.left.left.left = new TreeNode(7);
-//        head.left.left.right = new TreeNode(2);
-//
-//        head.right.left = new TreeNode(13);
-//        head.right.right = new TreeNode(4);
-//
-//        head.right.right.right = new TreeNode(1);
-
-//        TreeNode head = new TreeNode(1);
-//        head.left = new TreeNode(2);
-//        head.right = new TreeNode(3);
-
-        TreeNode head = new TreeNode(1);
-        head.right = new TreeNode(2);
-        head.right.right = new TreeNode(3);
-        head.right.right.right = new TreeNode(4);
-        head.right.right.right.right = new TreeNode(5);
-
-        boolean b = hasPathSum(head, 3);
-        System.out.println(b);
-    }
-
-    public static boolean hasPathSum(TreeNode root, int targetSum) {
-//        while (root != null || !stack.isEmpty()) {
-//            System.out.println("当前栈: " + stack);
-//            try {
-//                Thread.sleep(1000);
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//
+//        Stack<TreeNode> stack = new Stack<>();
+//        int sum = 0;
+////        TreeNode pre = null;
+//        Set<TreeNode> set = new HashSet<>();
+//        while (!stack.isEmpty() || root != null) {
 //            while (root != null) {
-//                stack.add(root);
 //                sum += root.val;
+//                stack.push(root);
 //                root = root.left;
 //            }
 //
-//            System.out.println("总和" + sum);
-//
-//            //如果这个节点不是叶子节点，那么不行
-//            root = stack.peek();
-//            if (pre != null && pre == root) {
-//                sum -= pre.val;
-//                stack.pop();
-//                root = stack.peek();
-//            }
-//
-//            if (root.left == null && root.right == null) {
-////                sum += root.val;
-//                if (sum == targetSum) {
+//            TreeNode pop = stack.pop();
+//            if (pop.left == null && pop.right == null) {
+//                if (sum == targetSum ) {
 //                    return true;
-//                } else {
-//                    sum -= root.val;
-//                    root = stack.pop();
 //                }
-//            } else {
-//                if (root.left != null && root.right != null) {
-//                    pre = root;
-//                }
+//                sum -= pop.val;
+//            } else if (pop.left != null && pop.right == null) {
+//                set.add(pop.left);
+//                sum -= pop.val;
+//                pop = stack.pop();
+//            } else if (pop.left == null && pop.right != null) {
+//
+//            } else if (pop.left != null && pop.right != null) {
+//                set.add(pop);
+//                sum -= pop.val;
+//                pop = stack.pop();
 //            }
 //
-//            root = root.right;
+//            root = pop.right;
 //        }
+//
+        return false;
 
-        Stack<TreeNode> stack = new Stack<>();
-        int sum = 0;
-        stack.push(root);
-        while (!stack.isEmpty()) {
-            TreeNode pop = stack.pop();
-            sum += pop.val;
-            if (pop.left == null && pop.right == null) {
-                if (targetSum == sum) {
-                    return true;
-                }
-            }
+    }
 
-            if (pop.right != null) {
-                stack.push(pop.right);
-            }
-
-            if (pop.left != null) {
-                stack.push(pop.left);
-            }
+    public boolean f(TreeNode root, int sum, int targetSum) {
+        if (root == null) {
+            return false;
         }
 
+        if (sum == targetSum && root.left == null && root.right == null) {
+            return true;
+        }
+
+        f(root.left, sum + root.left.val, targetSum);
+        sum -= root.left.val;
+        f(root.right, sum + root.right.val, targetSum);
         return false;
     }
 }
--- src/main/java/org/xqd/learning/leetcode/tree/RecoverBinarySearchTree.java
+++ src/main/java/org/xqd/learning/leetcode/tree/RecoverBinarySearchTree.java
@@ -2,13 +2,20 @@ package org.xqd.learning.leetcode.tree;
 
 import org.xqd.learning.leetcode.pojo.TreeNode;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 99. Recover Binary Search Tree
+ */
 public class RecoverBinarySearchTree {
 
     TreeNode first = null;
     TreeNode second = null;
     TreeNode last = null;
 
-    public void recoverTree(TreeNode root) {
+    public void recoverTree1(TreeNode root) {
         if (root == null) {
             return;
         }
@@ -29,5 +36,39 @@ public class RecoverBinarySearchTree {
         }
         last = root;
         dfs(root.right);
+    }
+
+
+    public void recoverTree(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        TreeNode a = null;
+        TreeNode b = null;
+
+        TreeNode dummy = root;
+        while (!stack.isEmpty() || root != null) {
+            //模拟在递归中一直向左遍历
+            while (root != null) {
+                stack.add(root);
+                root = root.left;
+//                list.add(root.val);
+            }
+            //已经走到最左的节点
+            TreeNode node = stack.peek();
+//            list.add(node.val);
+            if (pre != null && pre.val > node.val) {
+                if (a == null) {
+                    a = pre;
+                }
+                b = node;
+            }
+
+            pre = node;
+            root = node.right;
+        }
+
+        int temp = b.val;
+        b.val = a.val;
+        a.val = temp;
     }
 }
