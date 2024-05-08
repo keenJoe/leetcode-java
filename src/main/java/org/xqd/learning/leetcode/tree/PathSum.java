e07cc901fcf9916091b55d6abdf268e3e526e0c7
XuQidong <qidongxuca@gmail.com>
2024-05-08 22:32:58 +0800

    112

--- src/main/java/org/xqd/learning/leetcode/tree/PathSum.java
+++ src/main/java/org/xqd/learning/leetcode/tree/PathSum.java
@@ -2,6 +2,8 @@ package org.xqd.learning.leetcode.tree;
 
 import org.xqd.learning.leetcode.pojo.TreeNode;
 
+import java.util.Stack;
+
 /**
  * 112. Path Sum
  *
@@ -9,56 +11,37 @@ import org.xqd.learning.leetcode.pojo.TreeNode;
  */
 public class PathSum {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-//        if (root != null && root.val == targetSum && (root.left != null || root.right != null)) return false;
-//
-//        Stack<TreeNode> stack = new Stack<>();
-//        int sum = 0;
-////        TreeNode pre = null;
-//        Set<TreeNode> set = new HashSet<>();
-//        while (!stack.isEmpty() || root != null) {
-//            while (root != null) {
-//                sum += root.val;
-//                stack.push(root);
-//                root = root.left;
-//            }
-//
-//            TreeNode pop = stack.pop();
-//            if (pop.left == null && pop.right == null) {
-//                if (sum == targetSum ) {
-//                    return true;
-//                }
-//                sum -= pop.val;
-//            } else if (pop.left != null && pop.right == null) {
-//                set.add(pop.left);
-//                sum -= pop.val;
-//                pop = stack.pop();
-//            } else if (pop.left == null && pop.right != null) {
-//
-//            } else if (pop.left != null && pop.right != null) {
-//                set.add(pop);
-//                sum -= pop.val;
-//                pop = stack.pop();
-//            }
-//
-//            root = pop.right;
-//        }
-//
-        return false;
-
-    }
-
-    public boolean f(TreeNode root, int sum, int targetSum) {
         if (root == null) {
             return false;
         }
 
-        if (sum == targetSum && root.left == null && root.right == null) {
-            return true;
+        Stack<TreeNode> nodeStack = new Stack<>();
+        Stack<Integer> sumStack = new Stack();
+        TreeNode curr = root;
+        int sum = 0;
+
+        while (curr != null || !nodeStack.isEmpty()) {
+            while (curr != null) {
+                sum += curr.val;
+                nodeStack.push(curr);
+                sumStack.push(sum);
+                curr = curr.left;
+            }
+
+            curr = nodeStack.pop();
+            sum = sumStack.pop();
+
+            if (curr.left == null && curr.right == null && sum == targetSum) {
+                return true;
+            }
+
+            if (curr.right != null) {
+                curr = curr.right;
+            } else {
+                curr = null;
+            }
         }
 
-        f(root.left, sum + root.left.val, targetSum);
-        sum -= root.left.val;
-        f(root.right, sum + root.right.val, targetSum);
         return false;
     }
 }
