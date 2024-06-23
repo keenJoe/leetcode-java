46cb248aaf0fb6afef57340338d4ec8bd7a955f4
XuQidong <qidongxuca@gmail.com>
2024-06-23 12:52:24 +0800

    21

--- src/main/java/org/xqd/learning/leetcode/linkedlist2024/MergeTwoSortedLists.java
+++ src/main/java/org/xqd/learning/leetcode/linkedlist2024/MergeTwoSortedLists.java
@@ -25,8 +25,28 @@ public class MergeTwoSortedLists {
         return dummy.next;
     }
 
-    //写一个递归试试，真冥思苦想
+    /**
+     * 递归
+     * 两个链表合并，当前两个节点进行比较，小的节点在前。
+     * 那么小的next节点就是两个链表剩余部分的合并。也就是第一个链表的head.next 和第二个链表的head开始进行比较。
+     * 这样就开始了递归。
+     * <p>
+     * 终止条件就是两个链表全都是null
+     */
+
     public ListNode mergeTwoLists1(ListNode list1, ListNode list2) {
-        return null;
+        //一定要注意，如果其中一个链表是null，那么返回另外一个链表。等于把另外一个链表完全连接到next
+        if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        } else if (list1.val <= list2.val) {
+            list1.next = mergeTwoLists1(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+        //至于最后返回list1还是list2，由两个链表的头结点决定
     }
 }
